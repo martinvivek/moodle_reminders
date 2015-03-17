331bcd17396f52e2951c55378731ab1c53ffd1a8
Andreas Stocker <andreas@stockers.org>
2015-03-17 12:38:24 +0200

    Warnings displayed when no subscribed teachers can be found

--- lib.php
+++ lib.php
@@ -8,37 +8,38 @@ function local_moodle_reminders_cron() {
 
     $renderer = new \template_renderer();
 
-    echo 'Gathering data ... ';
+    echo "Gathering data ... \n";
 
     $teachers = \teacher\teacher::get_all();
 
-    echo 'Sending Emails ... ';
+    if (sizeof($teachers) == 0) {
+        echo "No subscribed Teachers Found! See: https://github.com/Arubaruba/moodle_reminders#how-to-subscribe-all-teachers\n";
+    } else {
+        echo 'Sending Emails ... ';
 
-    global $CFG;
-    $image_url = $CFG->wwwroot . '/local/moodle_reminders/templates/images/';
+        foreach ($teachers as $teacher) {
+            $email_html = $renderer->render('teacher_email.twig', 'teacher_email.css', (array)$teacher);
 
-    foreach ($teachers as $teacher) {
-        $email_html = $renderer->render('teacher_email.twig', 'teacher_email.css', (array)$teacher);
+            $mail = new PHPMailer();
+            $mail->isSendmail();
+            $mail->CharSet = 'UTF-8';
 
-        $mail = new PHPMailer();
-        $mail->isSendmail();
-        $mail->CharSet = 'UTF-8';
+            $mail->From = 'noreply@unic.ac.cy';
+            $mail->FromName = 'DLIT';
+            $mail->addAddress('andreas_stocker@outlook.com', $teacher->name);
+            $mail->isHTML(true);
 
-        $mail->From = 'noreply@unic.ac.cy';
-        $mail->FromName = 'DLIT';
-        $mail->addAddress('andreas_stocker@outlook.com' , $teacher->name);
-        $mail->isHTML(true);
+            $mail->Subject = 'Weekly Course Report';
+            $mail->Body = $email_html;
 
-        $mail->Subject = 'Weekly Course Report';
-        $mail->Body = $email_html;
-
-        if (!$mail->send()) {
-            echo 'Message could not be sent.';
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            echo 'Message has been sent (' . $teacher->email . ')';
+            if (!$mail->send()) {
+                echo 'Message could not be sent.';
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            } else {
+                echo 'Message has been sent (' . $teacher->email . ')';
+            }
         }
-    }
 
-    echo 'Sent Emails';
+        echo 'Sent Emails';
+    }
 }
--- tests/render_test_email.php
+++ tests/render_test_email.php
@@ -10,9 +10,16 @@ require_once(__DIR__ . '/../teacher/teacher.php');
 
 $teachers = teacher::get_all();
 
-// Don't use caching
-$renderer = new \template_renderer(false);
+if (sizeof($teachers) == 0) {
+    echo '
+    <style>body{text-align: center; font-size: 20px;}</style>
+    <h4>You need at least 1 subscribed teacher to view the test email!</h4>
+    <a target="_blank" href="https://github.com/Arubaruba/moodle_reminders#how-to-subscribe-all-teachers">Github README: How to Subscribe <b>ALL</b> Teachers</a>
+    ';
+} else {
+    $data = (array)array_values($teachers)[0];
+    // Don't use caching
+    $renderer = new \template_renderer(false);
+    echo $renderer->render('teacher_email.twig', 'teacher_email.css', $data);
+}
 
-$data = (array) array_values($teachers)[0];
-
-echo $renderer->render('teacher_email.twig', 'teacher_email.css', $data);
