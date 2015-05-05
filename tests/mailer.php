f5b5af865c6ccaa951d37a2d9a895c519f622822
Andreas Stocker <andreas@stockers.org>
2015-05-05 11:14:46 +0300

    Mailer will only gather data when sending emails

--- tests/mailer.php
+++ tests/mailer.php
@@ -18,11 +18,6 @@ require_login();
 // Change the php timeout for this session
 set_time_limit(6000); # One hour
 
-global $USER;
-
-$teacher_factory = new teacher_factory();
-$teachers = $teacher_factory->load_records('teacher_by_id.sql', array('teacher_id' => $USER->id));
-
 // Set Renderer Options
 $PAGE->set_pagelayout('report'); // To add the sidebar
 $PAGE->set_title(get_string('pluginname', 'local_moodle_reminders'));
@@ -30,44 +25,44 @@ $PAGE->set_heading('Mailer Test');
 
 echo $OUTPUT->header();
 
-$teacher = array_values($teachers)[0];
-if (!$teacher->courses) {
-    echo '<h2>Error: You are not teaching an active course</h2> ';
-} else {
-    echo '
+echo '
 <h3>Send your course report as an email to:</h3>
 <form action="" method="get">
   <textarea style="width:250px; height:100px" name="emails">' . ($_GET['emails'] ? htmlspecialchars($_GET['emails']) : $teacher->email) . '</textarea>
   <button type="submit">Send Emails!</button>
 </form>
 ';
-    $addresses = explode("\n", htmlspecialchars($_GET['emails']));
+$addresses = explode("\n", htmlspecialchars($_GET['emails']));
 
-    if ($addresses) {
+if ($_GET['emails']) {
 
-        $renderer = new template_renderer(false);
-        $email_html = $renderer->render_email('teacher_email.twig', 'teacher_email.css', (array)$teacher);
+    global $USER;
 
-        $mail = new PHPMailer();
-        $mail->isSendmail();
-        $mail->CharSet = 'UTF-8';
+    $teacher_factory = new teacher_factory();
+    $teachers = $teacher_factory->load_records('teacher_by_id.sql', array('teacher_id' => $USER->id));
 
-        $mail->From = 'noreply@unic.ac.cy';
-        $mail->FromName = 'DLIT';
-        foreach($addresses as &$address) {
-            $mail->addAddress($address, 'Teacher Email Recipient');
-        }
-        $mail->isHTML(true);
+    $renderer = new template_renderer(false);
+    $email_html = $renderer->render_email('teacher_email.twig', 'teacher_email.css', (array)$teacher);
 
-        $mail->Subject = 'Weekly Course Report';
-        $mail->Body = $email_html;
+    $mail = new PHPMailer();
+    $mail->isSendmail();
+    $mail->CharSet = 'UTF-8';
 
-        if (!$mail->send()) {
-            echo "Message could not be sent.<br>";
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            echo "Messages Sent \n \n";
-        }
+    $mail->From = 'noreply@unic.ac.cy';
+    $mail->FromName = 'DLIT';
+    foreach ($addresses as &$address) {
+        $mail->addAddress($address, 'Teacher Email Recipient');
+    }
+    $mail->isHTML(true);
+
+    $mail->Subject = 'Weekly Course Report';
+    $mail->Body = $email_html;
+
+    if (!$mail->send()) {
+        echo "Message could not be sent.<br>";
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo "Messages Sent \n \n";
     }
 }
 
