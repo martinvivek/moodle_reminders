8331652137c3f800cbbd0e0ebbd354b4d6585469
Andreas Stocker <andreas@stockers.org>
2015-05-05 13:09:50 +0300

    Temporarily removed logstore usage for testing

--- sql/factory/student.sql
+++ sql/factory/student.sql
@@ -1,20 +1,20 @@
-SELECT SQL_NO_CACHE
+SELECT
 /* Prevent score inflation through rapid page refreshing
    By only counting a specific page view every 30 seconds */
   mdl_user.id                                               AS id,
   CONCAT_WS(' ', mdl_user.firstname, mdl_user.lastname)     AS name,
   mdl_user.email                                            AS email,
   FROM_UNIXTIME(MAX(mdl_logstore_standard_log.timecreated)) AS last_access_date,
-  SUM(
-      CASE mdl_logstore_standard_log.action
-      #         WHEN 'viewed' THEN 1
-            #SQL action_cases
-      ELSE 0 END)
-  /* we multiply by the percentage of events that where created within 3 seconds distance of another */
-  * (COUNT(DISTINCT ROUND(mdl_logstore_standard_log.timecreated / 3)) / COUNT(mdl_logstore_standard_log.id))
-  /* We want the score per week */
-  / (DATEDIFF(NOW(), FROM_UNIXTIME(mdl_course.startdate)) / 7)
-                                                            AS score
+#   SUM(
+#       CASE mdl_logstore_standard_log.action
+#       #         WHEN 'viewed' THEN 1
+#             #SQL action_cases
+#       ELSE 0 END)
+#   /* we multiply by the percentage of events that where created within 3 seconds distance of another */
+#   * (COUNT(DISTINCT ROUND(mdl_logstore_standard_log.timecreated / 3)) / COUNT(mdl_logstore_standard_log.id))
+#   /* We want the score per week */
+#   / (DATEDIFF(NOW(), FROM_UNIXTIME(mdl_course.startdate)) / 7)
+                                                           0.5 AS score
 FROM mdl_user
   JOIN mdl_course ON mdl_course.id = :course_id
   JOIN mdl_context ON mdl_context.contextlevel = 50 AND mdl_context.instanceid = mdl_course.id
